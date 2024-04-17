342f62eed045b7601b80c624b122e0a470691219
julianKim11 <juliankim0911@gmail.com>
2024-04-17 19:33:37 -0300

    movimiento de la paleta

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //Velocidad de la paleta
+    [SerializeField] private float _speed = 5f;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        //Entrada del teclado
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        //Calculo del movimiento
+        Vector3 movement = new Vector3 (horizontalMovement, 0f, 0f) * _speed * Time.deltaTime;
+        //Aplicar el movimiento
+        transform.Translate(movement);
     }
 }
