b0c8e887f891f623307ead8d10cdb61c6a5d0e83
julianKim11 <juliankim0911@gmail.com>
2024-06-05 14:49:44 -0300

    pantalla de victoria y derrota

--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -6,10 +6,16 @@ using UnityEngine;
 public class HUD : MonoBehaviour
 {
     public GameObject[] lives;
+    public GameObject endScreen;
     public TextMeshProUGUI level;
+    public TextMeshProUGUI result;
     public void ChangeLevel(string newText)
     {
         level.text = newText;
+    }
+    public void ChangeResult(string newText)
+    {
+        result.text = newText;
     }
     public void DeactivateLife(int index)
     {
