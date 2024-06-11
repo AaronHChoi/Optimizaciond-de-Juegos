3071f392c9e2aa0cb0fbd92e627e57de08cf3107
julianKim11 <juliankim0911@gmail.com>
2024-06-11 01:27:48 -0300

    AudioManager 1

---
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource sfxSource;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        } else
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void PlayMusic(AudioClip clip)
+    {
+        musicSource.clip = clip;
+        musicSource.Play();
+    }
+    public void StopMusic()
+    {
+        musicSource.Stop();
+    }
+    public void SetMusicVolume(float volume)
+    {
+        musicSource.volume = volume;
+    }
+    public void PlaySFX(AudioClip clip)
+    {
+        sfxSource.PlayOneShot(clip);
+    }
+    public void SetSFXVolume(float volume)
+    {
+        sfxSource.volume = volume;
+    }
+}
